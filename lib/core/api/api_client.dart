c3be1f8ac107e55bf30b04710f1b9c7aeb4ebf27
Ayabonga <ayabongabooi2@gmail.com>
2025-01-04 23:20:20 +0200

    chore: code clean up

--- lib/core/api/api_client.dart
+++ lib/core/api/api_client.dart
@@ -5,20 +5,20 @@ import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class ApiClient {
   Dio getDio({bool tokenInterceptor = false}) {
-    Dio dio = Dio();
+    final dio = Dio();
     dio.options.baseUrl = ApiConfig.BASE_URL_FOR_DIO;
 
     if (tokenInterceptor) {
       dio.interceptors.add(TokenInterceptor(dio: dio));
     }
-    dio.interceptors.add(PrettyDioLogger(
-      responseHeader: true,
-      responseBody: true,
-      requestHeader: true,
-      requestBody: true,
-      request: true,
-      compact: false,
-    ));
+    dio.interceptors.add(
+      PrettyDioLogger(
+        responseHeader: true,
+        requestHeader: true,
+        requestBody: true,
+        compact: false,
+      ),
+    );
 
     return dio;
   }
