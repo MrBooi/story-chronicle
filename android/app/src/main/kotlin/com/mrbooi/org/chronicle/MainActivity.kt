17c588ebab8180d7a20ef2ceacd5146be0ee5d5e
Ayabonga <ayabongabooi2@gmail.com>
2025-01-04 22:18:00 +0200

    project setup

---
+++ android/app/src/main/kotlin/com/mrbooi/org/chronicle/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mrbooi.org.chronicle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:chronicle/app/app.dart';
+import 'package:chronicle/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
