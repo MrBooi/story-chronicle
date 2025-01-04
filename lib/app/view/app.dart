cafc06ec30b68cad10d4922c0a2f20e1d0ba5e9f
Ayabonga <ayabongabooi2@gmail.com>
2025-01-05 00:46:11 +0200

    wip: register user bloc

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,18 +1,24 @@
+import 'package:chronicle/core/di/get_it.dart';
 import 'package:chronicle/core/router/app_router.dart';
 import 'package:chronicle/core/theme/app_theme.dart';
+import 'package:chronicle/features/auth/presentation/bloc/bloc/user_bloc.dart';
 import 'package:chronicle/l10n/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      theme: AppTheme.getTheme(),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
+    return BlocProvider(
+      create: (context) => getIt<UserBloc>(),
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        theme: AppTheme.getTheme(),
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+      ),
     );
   }
 }
